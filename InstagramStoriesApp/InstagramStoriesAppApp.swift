8ee4c349eeaba5857f2d68c70d027d5fbdc644d6
Muhammad Usman <usmans1020@gmail.com>
2025-07-26 12:09:43 +0200

    Add Initial project

---
+++ InstagramStoriesApp/InstagramStoriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoriesAppApp.swift
+//  InstagramStoriesApp
+//
+//  Created by Usman on 26.07.25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramStoriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstagramStoriesAppTests/InstagramStoriesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoriesAppTests.swift
+//  InstagramStoriesAppTests
+//
+//  Created by Usman on 26.07.25.
+//
+
+import Testing
+@testable import InstagramStoriesApp
+
+struct InstagramStoriesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
